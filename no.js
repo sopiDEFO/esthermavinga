6d21b509fd4dd98f0dfc9217e839a2c4ab1f4fd1
sopiDEFO <kamdemsobdiefblerio@gmail.com>
2024-10-29 20:58:41 +0100

    esther
    amour

---
+++ no.js
@@ -0,0 +1,11 @@
+vanillaTilt.init(document.querySelectorAll(".card"),  {
+    mox:15,
+    speed:400,
+    glore:true,
+    "max-glare": .5
+});
+if (container) {
+    else(package)
+} else {
+    
+}
